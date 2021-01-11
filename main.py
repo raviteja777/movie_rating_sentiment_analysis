7258e423dff7e891186fa71e5b58f856422b314a
Raviteja <raviteja777@gmail.com>
2021-01-11 12:52:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,17 +3,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import os
-import numpy as np
 import datetime
+from logging import Logger
 from file_processing import FileProcessor
 from model_operations import Modeller
-from utils import Utils
+from utils import FileUtils, LoggerUtil
 from sklearn.metrics import confusion_matrix, classification_report
 
 data_path = "data"
 save_model_path = "saved_models"
-utils = Utils()
-
+session_id = str(datetime.datetime.now().timestamp())
+utils = FileUtils()
+logger = LoggerUtil(session_id,'files')
 
 def prepare_dataset(dir_path, is_train=True):
     processor = FileProcessor(dir_path, is_train)
@@ -34,22 +35,30 @@ def display_metrics(results_data):
     print("======Confusion Matrix=====")
     print(c_matrix)
     print(classification_report(results_data["actual"], results_data["predicted"]))
+    logger.log_message(results_data.head(20))
+    logger.log_message(c_matrix)
+    logger.log_message(classification_report(results_data["actual"], results_data["predicted"]))
 
 
 if __name__ == '__main__':
     # training
+    logger.log_message("start training")
     utils.update_files_list(os.path.join(data_path, "train"))
     train_data = prepare_dataset(utils.get_files())
+    logger.log_message("Train data procession complete")
     modeller = prepare_model(train_data)
 
     # save model
-    tm = datetime.datetime.now()
-    modeller.get_model().save(os.path.join(save_model_path, "model_" + str(tm.timestamp())))
+    logger.log_message("Training model saved at model_"+session_id)
+    modeller.get_model().save(os.path.join(save_model_path, "model_" + session_id))
 
     # testing
+    logger.log_message("start testing")
     utils.update_files_list(os.path.join(data_path, "test"))
     test_data = prepare_dataset(utils.get_files(), False)
+    logger.log_message("Test data procession complete")
     predicted_results = modeller.predict_output(test_data)
 
     # display metrics
     display_metrics(predicted_results)
+    logger.log_message("==========Execution completed ==============")
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 # helper modules
 import os
 import re
+import logging
 
 
-class Utils:
+class FileUtils:
     dirs = []
     files = []
 
@@ -21,6 +22,17 @@ class Utils:
 
     def get_files(self):
         return self.files
+
+
+class LoggerUtil:
+
+    def __init__(self,session,log_file_path):
+        self.log_file = os.path.join(log_file_path,session+'.log')
+        self.logger = logging
+        self.logger.basicConfig(filename=self.log_file,datefmt='%y-%m-%d',level='INFO')
+
+    def log_message(self,message):
+        self.logger.log(level=logging.INFO, msg=message)
 
 # get_files_list("data/train")
 # print(files)
